95922b3270d9a281112597821a5f68814edf47d4
NoahPeeters <noah@noahpeeters.de>
2020-05-30 23:05:04 +0200

    Add server target

--- GermanLaws/Package.swift
+++ GermanLaws/Package.swift
@@ -10,13 +10,17 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "GermanLaws",
-            targets: ["GermanLaws", "GermanLawsApi"])
+            targets: ["GermanLaws", "GermanLawsApi"]),
+        .executable(name: "GermanLawsServer", targets: ["GermanLawsServer"])
     ],
     dependencies: [
         .package(url: "https://github.com/MaxDesiatov/XMLCoder.git", from: "0.11.1"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.11")
     ],
     targets: [
+        .target(
+            name: "GermanLawsServer",
+            dependencies: []),
         .target(
             name: "GermanLaws",
             dependencies: []),
---
+++ GermanLaws/Sources/GermanLawsServer/main.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Noah Peeters on 30.05.20.
+//
+
+import Foundation
+
+print("Hello World")
